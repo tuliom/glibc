51d80cdb3a2df672da47a588d0ae1fdc2bfdf0e7
Paul E. Murphy <murphyp@linux.vnet.ibm.com>
2016-07-25 13:10:00 -0500

    float128: Add strtof128, wcstof128, and related functions.
    This is relatively straighfoward with the exception of
    needing to update the power of ten tables used by the
    common implementation when long double is not the most
    expressive real type.
    
    The implementations are contained with sysdeps/ieee754/float128 as
    they are only built when _Float128 is enabled within libc/m.
    	* include/gmp.h (__mpn_construct_float128): New declaration.
    	* include/stdlib.h (__strtof128_l): Likewise.
    	(__strtof128_nan): Likewise.
    	(__wcstof128_nan): Likewise.
    	(__strtof128_internal): Likewise.
    	(____strtof128_l_internal): Likewise.
    	* include/wchar.h (__wcstof128_l): Likewise.
    	(__wcstof128_internal): Likewise.
    
    	* stdlib/Makefile (bug-strtod2): Link libm too.
    
    	* stdlib/fpioconst.c (HAVE_EXTENDED_RANGE): New macro
    	for testing how big the power of ten table should be.
    	[TENS_P11_IDX]: Include if above is true.
    	[TENS_P12_IDX]: Include if above is true.
    	[TENS_P13_IDX]: Include if above is true.
    	[TENS_P14_IDX]: Include if above is true.
    	* stdlib/fpioconst.h (FPIOCONST_POW10_ARRAY_SIZE):
    	Use larger table if _Float128 is supported.
    
    	* stdlib/stdlib.h (strtof128): New declaration.
    	(strtof128_l): Likewise.
    
    	* stdlib/tst-strtod-nan-locale-main.c: Updated
    	to use tst-strtod.h macros to ensure float128
    	gets tested too.
    
    	* stdlib/tst-strtod-round-skeleton.c (CHOOSE_f128):
    	New macro.
    
    	* stdlib/tst-strtod.h (_GEN_F128): New macro.
    	(_GEN): Likewise.
    	(_GENx): Likewise.
    	(_DO): Likewise.
    	(_DOx): Likewise.
    	(GEN_TEST_STRTOD): Update to optionally include
    	_Float128 in the tests.
    	(STRTOD_TEST_FOREACH): Likewise.
    
    	* sysdeps/ieee754/float128/Makefile: Insert
    	new strtof128 and wcstof128 functions into
    	libc.
    
    	* sysdeps/ieee754/float128/Versions: Add
    	exports for the above new functions.
    
    	* sysdeps/ieee754/float128/mpn2float128.c: New file.
    	* sysdeps/ieee754/float128/strtod_nan_float128.h:
    	New file.
    	* sysdeps/ieee754/float128/strtof128.c: New file.
    	* sysdeps/ieee754/float128/strtof128_l.c: New file.
    	* sysdeps/ieee754/float128/strtof128_nan.c: New file.
    	* sysdeps/ieee754/float128/wcstof128.c: New file.
    	* sysdeps/ieee754/float128/wcstof128_l.c: New file.
    	* sysdeps/ieee754/float128/wcstof128_nan.c: New fike.
    	* wcsmbs/Makefile: (CFLAGS-wcstof128.c): Append
    	strtof-CFLAGS.
    	(CFLAGS-wcstof128_l): Likewise.
    
    	* wcsmbs/wchar.h (wcstof128): New declaration.
    	(wcstof128_l): Likewise.

--- stdlib/stdlib.h
+++ stdlib/stdlib.h
@@ -128,6 +128,13 @@ extern long double strtold (const char *__restrict __nptr,
      __THROW __nonnull ((1));
 #endif
 
+#if __USE_FLOAT128
+/* Likewise for the '_Float128' format  */
+extern _Float128 strtof128 (const char *__restrict __nptr,
+		      char **__restrict __endptr)
+     __THROW __nonnull ((1));
+#endif
+
 /* Convert a string to a long integer.  */
 extern long int strtol (const char *__restrict __nptr,
 			char **__restrict __endptr, int __base)
@@ -235,6 +242,14 @@ extern long double strtold_l (const char *__restrict __nptr,
 			      char **__restrict __endptr,
 			      __locale_t __loc)
      __THROW __nonnull ((1, 3));
+
+# if __USE_FLOAT128
+extern _Float128 strtof128_l (const char *__restrict __nptr,
+			      char **__restrict __endptr,
+			      __locale_t __loc)
+     __THROW __nonnull ((1, 3));
+# endif
+
 #endif /* GNU */
 
 
